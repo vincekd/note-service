e2c9bba51aad68794166c23610b7b043a9eda3a9
vincekd <vincekd@gmail.com>
2016-01-18 20:16:53 -0500

    Filter for less css cleanup

--- src/main/groovy/com/sticklet/core/config/WebMvcConfiguration.groovy
+++ src/main/groovy/com/sticklet/core/config/WebMvcConfiguration.groovy
@@ -88,18 +88,4 @@ class WebMvcConfiguration extends WebMvcConfigurationSupport {
     public LessFilter lessFilter() {
         new LessFilter()
     }
-
-//    @Bean
-//    public WroFilter wroFilter() {
-//        //new Wro4jFilter(super.defaultWroProperties, new Wro4jManagerFactory(super.cacheManager), false);
-//        new WroFilter()
-//    }
-
-    //    @Bean
-    //    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-    //        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
-    //        builder.serializationInclusion(JsonInclude.Include.NON_NULL)
-    //        builder.createXmlMapper = true
-    //        return builder
-    //    }
 }
