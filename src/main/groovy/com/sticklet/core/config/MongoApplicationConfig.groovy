167ff97306b261fbefa12befa2de8725570dec1a
vincekd <vincekd@gmail.com>
2016-01-19 18:15:27 -0500

    provide for mongoDB auth

--- src/main/groovy/com/sticklet/core/config/MongoApplicationConfig.groovy
+++ src/main/groovy/com/sticklet/core/config/MongoApplicationConfig.groovy
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
+import org.springframework.data.authentication.UserCredentials
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
@@ -16,11 +17,10 @@ import org.springframework.data.mongodb.core.convert.DbRefResolver
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 import com.mongodb.Mongo
 import com.mongodb.MongoClient
-import com.sticklet.core.repository.base.BaseRepoFactoryBean
+import com.mongodb.WriteConcern
 
 @Configuration
 @EnableMongoAuditing
@@ -38,6 +38,12 @@ class MongoApplicationConfig extends AbstractMongoConfiguration {
     @Value("\${spring.data.mongodb.port}")
     int dbPort
 
+    @Value("\${spring.data.mongodb.username}")
+    String dbUser
+
+    @Value("\${spring.data.mongodb.password}")
+    String dbPass
+
     @Override
     public Mongo mongo() throws Exception {
         return new MongoClient(dbHost, dbPort)
@@ -53,47 +59,54 @@ class MongoApplicationConfig extends AbstractMongoConfiguration {
         "com.sticklet"
     }
 
-    @Bean
-    @Override
-    public CustomConversions customConversions() {
-        Reflections reflections = new Reflections("com.sticklet")
-        //Set<Class> readClasses  = reflections.getSubTypesOf(BaseReadConverter.class)
-        //Set<Class> writeClasses = reflections.getSubTypesOf(BaseWriteConverter.class)
-
-        //logger.info "Readers: {}", readClasses
-        //logger.info "Writers: {}", writeClasses
-
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory())
-        refResolver = dbRefResolver
-
-        List<Converter<?, ?>> converterList = new ArrayList<Converter<?, ?>>()
-        //        converterList.addAll(readClasses.collect { Class c ->
-        //            def newC = c.newInstance()
-        //            newC.dbRefResolver = dbRefResolver
-        //            newC
-        //        })
-        //        converterList.addAll(writeClasses.collect { Class c -> c.newInstance() })
-
-        return new CustomConversions(converterList)
-    }
+    //    @Bean
+    //    @Override
+    //    public CustomConversions customConversions() {
+    //        Reflections reflections = new Reflections("com.sticklet")
+    //        //Set<Class> readClasses  = reflections.getSubTypesOf(BaseReadConverter.class)
+    //        //Set<Class> writeClasses = reflections.getSubTypesOf(BaseWriteConverter.class)
+    //
+    //        //logger.info "Readers: {}", readClasses
+    //        //logger.info "Writers: {}", writeClasses
+    //
+    //        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory())
+    //        refResolver = dbRefResolver
+    //
+    //        List<Converter<?, ?>> converterList = new ArrayList<Converter<?, ?>>()
+    //        //        converterList.addAll(readClasses.collect { Class c ->
+    //        //            def newC = c.newInstance()
+    //        //            newC.dbRefResolver = dbRefResolver
+    //        //            newC
+    //        //        })
+    //        //        converterList.addAll(writeClasses.collect { Class c -> c.newInstance() })
+    //
+    //        return new CustomConversions(converterList)
+    //    }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongoDbFactory(), mongoConverter())
+
+        UserCredentials userCredentials = new UserCredentials(dbUser, dbPass)
+        MongoTemplate mongoTemplate = new MongoTemplate(mongo(), getDatabaseName(), userCredentials)
+        mongoTemplate.setWriteConcern(WriteConcern.SAFE)
+
+        //MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory(), mongoConverter())
+
+        mongoTemplate
     }
 
-    @Bean
-    public MappingMongoConverter mongoConverter() throws Exception {
-        MongoMappingContext mappingContext = new MongoMappingContext()
-
-        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory())
-
-        MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mappingContext)
-
-        mongoConverter.setCustomConversions(customConversions())
-
-        mongoConverter
-    }
+    //    @Bean
+    //    public MappingMongoConverter mongoConverter() throws Exception {
+    //        MongoMappingContext mappingContext = new MongoMappingContext()
+    //
+    //        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory())
+    //
+    //        MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mappingContext)
+    //
+    //        mongoConverter.setCustomConversions(customConversions())
+    //
+    //        mongoConverter
+    //    }
 
     @Bean
     public AuditorAware<String> myAuditorProvider() {
