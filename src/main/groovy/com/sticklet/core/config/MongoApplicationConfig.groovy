12bef3af0bf3db73192aade70fa6511acc7bf170
vincekd <vincekd@gmail.com>
2016-01-19 20:26:09 -0500

    mongodb authorization issue fix

--- src/main/groovy/com/sticklet/core/config/MongoApplicationConfig.groovy
+++ src/main/groovy/com/sticklet/core/config/MongoApplicationConfig.groovy
@@ -1,26 +1,19 @@
 package com.sticklet.core.config
 
-import org.reflections.Reflections
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.convert.converter.Converter
-import org.springframework.data.authentication.UserCredentials
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.convert.CustomConversions
 import org.springframework.data.mongodb.core.convert.DbRefResolver
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 import com.mongodb.Mongo
 import com.mongodb.MongoClient
-import com.mongodb.WriteConcern
+import com.mongodb.MongoClientURI
 
 @Configuration
 @EnableMongoAuditing
@@ -31,22 +24,22 @@ class MongoApplicationConfig extends AbstractMongoConfiguration {
 
     @Value("\${spring.data.mongodb.databaseName}")
     String dbName
-
     @Value("\${spring.data.mongodb.host}")
     String dbHost
-
     @Value("\${spring.data.mongodb.port}")
     int dbPort
-
     @Value("\${spring.data.mongodb.username}")
     String dbUser
-
     @Value("\${spring.data.mongodb.password}")
     String dbPass
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient(dbHost, dbPort)
+        logger.info "mongodb: $dbName, user: $dbUser"
+        MongoClientURI uri  = new MongoClientURI("mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}")
+        //new MongoClient(new ServerAddress(dbHost, dbPort),
+        //     [MongoCredential.createCredential(dbUser, dbHost, dbPass.toCharArray())])
+        new MongoClient(uri)
     }
 
     @Override
@@ -59,54 +52,10 @@ class MongoApplicationConfig extends AbstractMongoConfiguration {
         "com.sticklet"
     }
 
-    //    @Bean
-    //    @Override
-    //    public CustomConversions customConversions() {
-    //        Reflections reflections = new Reflections("com.sticklet")
-    //        //Set<Class> readClasses  = reflections.getSubTypesOf(BaseReadConverter.class)
-    //        //Set<Class> writeClasses = reflections.getSubTypesOf(BaseWriteConverter.class)
-    //
-    //        //logger.info "Readers: {}", readClasses
-    //        //logger.info "Writers: {}", writeClasses
-    //
-    //        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory())
-    //        refResolver = dbRefResolver
-    //
-    //        List<Converter<?, ?>> converterList = new ArrayList<Converter<?, ?>>()
-    //        //        converterList.addAll(readClasses.collect { Class c ->
-    //        //            def newC = c.newInstance()
-    //        //            newC.dbRefResolver = dbRefResolver
-    //        //            newC
-    //        //        })
-    //        //        converterList.addAll(writeClasses.collect { Class c -> c.newInstance() })
-    //
-    //        return new CustomConversions(converterList)
-    //    }
-
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-
-        UserCredentials userCredentials = new UserCredentials(dbUser, dbPass)
-        MongoTemplate mongoTemplate = new MongoTemplate(mongo(), getDatabaseName(), userCredentials)
-        mongoTemplate.setWriteConcern(WriteConcern.SAFE)
-
-        //MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory(), mongoConverter())
-
-        mongoTemplate
+        new MongoTemplate(mongo(), getDatabaseName())
     }
-
-    //    @Bean
-    //    public MappingMongoConverter mongoConverter() throws Exception {
-    //        MongoMappingContext mappingContext = new MongoMappingContext()
-    //
-    //        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory())
-    //
-    //        MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mappingContext)
-    //
-    //        mongoConverter.setCustomConversions(customConversions())
-    //
-    //        mongoConverter
-    //    }
 
     @Bean
     public AuditorAware<String> myAuditorProvider() {
